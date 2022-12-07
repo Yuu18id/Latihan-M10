6d3e09e56a6b9bb18e5305dea29a4e9d7ba2d954
Bayu Arma Praja <100572149+Yuu18id@users.noreply.github.com>
2022-12-07 16:19:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql2');
+
+app.use(bodyParser.json());
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'crud_db'
+});
+
+const middleware = (req, res, next) => {
+    console.log(`Request URL : ${req.url}\nRequest Type : ${req.method}`)
+    next()
+}
+
+conn.connect((err) => {
+    if(err) throw err;
+    console.log('Mysql Connected...');
+});
+
+app.use(middleware)
+
+app.get('/api/products', (req, res) => {
+    let sql = "SELECT * FROM product";
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status":200, "error": null, "response": results}))
+    });
+});
+
+app.get('/api/products/:id', (req, res) => {
+    let sql = "SELECT * FROM product WHERE product_id="+req.params.id;
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status":200, "error": null, "response": results}))
+    });
+});
+
+app.post('/api/products', (req, res) => {
+    let data = {product_name: req.body.product_name, product_price: req.body.product_price};
+    let sql = "INSERT INTO product SET ?";
+    let query = conn.query(sql, data,(err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status":200, "error": null, "response": "Insert data success"}))
+    });
+});
+
+app.put('/api/products/:id', (req, res) => {
+    let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.params.id;
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status":200, "error": null, "response": "Update data success"}))
+    });
+});
+
+app.delete('/api/products/:id', (req, res) => {
+    let sql = "DELETE FROM product WHERE product_id="+req.params.id+"";
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        res.send(JSON.stringify({"status":200, "error": null, "response": "Delete data success"}))
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000...')
+})
